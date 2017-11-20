9b576c78f163a9d5d8c7cdf6d56a8486c36d95de
Arianna Lanz <ariannalanz@gmail.com>
2017-11-20 11:28:13 -0500

    didn't save things

--- app/containers/LoggedInPage.js
+++ app/containers/LoggedInPage.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
 import LoggedIn from '../components/LoggedIn';
 import { bindActionCreators } from 'redux';
-import userActions from '../actions/user';
+// import userActions from '../actions/user';
+import {userLogin} from '../reducers/user';
 import { push } from 'react-router-redux';
 
 
@@ -10,7 +11,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => { // eslint-disable-line no-unused-vars
-  const user = bindActionCreators(userActions, dispatch);
+  const user = bindActionCreators(userLogin, dispatch);
   return {
     onLogout: () => {
       user.login();
