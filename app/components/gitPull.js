45163a7184e6320a31bf573cfc0a371274c5c979
sreepriya <sreepriyav@live.com>
2017-11-21 18:06:42 -0500

    changes made

---
+++ app/components/gitPull.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+export default class gitPull extends Component {
+
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <div >
+        <h1>hello</h1>
+
+      </div>
+    );
+  }
+}
