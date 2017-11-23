f95c5a2f9712d851aee45cd8c07e3f386fd90ffe
sreepriya <sreepriyav@live.com>
2017-11-23 13:48:08 -0500

    fetch done

--- app/nodegit/fetch.js
+++ app/nodegit/fetch.js
@@ -1,12 +1,6 @@
-//const git = require('simple-git')('/Users/sreepriyav/Desktop/seniors/test/dummy');
- 
 export const fetch = (path, branch) =>{
   require('simple-git')(`${path}`).fetch('origin', `${branch}`, ()=>{
     console.log('finished fetching ');
   });
-
-    
-   
-  // git.pull('origin', 'master', {'--no-rebase': null});
 
 };
