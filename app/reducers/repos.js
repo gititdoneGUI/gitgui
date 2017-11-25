972774ff8ca36a03787aff87551456491cd930ef
sreepriya <sreepriyav@live.com>
2017-11-24 23:13:58 -0500

    error handling done

--- app/reducers/repos.js
+++ app/reducers/repos.js
@@ -3,10 +3,10 @@ import { action } from '../actions/repos';
 //REDUCER
 export default function(state = [], action) {
   switch (action.type) {
-    case 'GET_REPOS':
-      return action.repos;
+  case 'GET_REPOS':
+    return action.repos;
 
-    default:
-      return state;
+  default:
+    return state;
   }
 }
