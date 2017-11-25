ca283b97045514f64b3e89031db3922a15f5ef20
sreepriya <sreepriyav@live.com>
2017-11-24 22:49:06 -0500

    error handling

--- app/nodegit/clone.js
+++ app/nodegit/clone.js
@@ -1,6 +1,7 @@
 export const clone = (path, remote) =>{
-  require('simple-git')(`${path}`).clone(`${remote}`,()=>{
-    console.log(' Clonning Successfully Completed ');
-  });
+  require('simple-git/promise')(`${path}`).clone(`${remote}`)
+    .then(()=> console.log(' Clonning Successfully Completed '))
+    .catch((err) => console.log('An Error Occurred: ',err));
 
 };
+
