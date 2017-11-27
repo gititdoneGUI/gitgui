47f3aeb4b8a2586b528607bc6656550717f7b81d
sreepriya <sreepriyav@live.com>
2017-11-27 16:05:40 -0500

    working

--- app/components/Push.js
+++ app/components/Push.js
@@ -6,30 +6,46 @@ class Push extends Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      clicked: false
+    };
     this.handlePullClick = this.handlePullClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handlePullClick(event) {
     event.preventDefault();
     push(this.props.userPath, event.target.branch.value);
+    this.setState({clicked: false});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({clicked: true});
   }
 
   render() {
     return (
-
-      <form onSubmit={this.handlePullClick} className="form-group">
-        <label>Branch to push into : </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="haxor99"
-          name="branch"
-        />
-        <button className="btn btn-large btn-primary">
+      <div>
+        { !this.state.clicked && <button className="btn btn-large btn-primary" onClick={this.handleSubmit}>
           <span className="icon icon-down-circled icon-text"></span>
-        Push
-        </button>
-      </form>
+           Push
+        </button>}
+        { this.state.clicked &&
+        <form onSubmit={this.handlePullClick} className="form-group">
+          <label>Branch to push into : </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="haxor99"
+            name="branch"
+          />
+          <button type="submit" className="btn btn-large btn-primary">
+            <span className="icon icon-down-circled icon-text"></span>
+            Submit Push
+          </button>
+        </form>
+        }
+      </div>
     );
   }
 }
