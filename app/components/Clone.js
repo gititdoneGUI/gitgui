15bd06df9ff25fc836af4da3e680d02bd50aad0e
sreepriya <sreepriyav@live.com>
2017-11-27 14:41:09 -0500

    done

--- app/components/Clone.js
+++ app/components/Clone.js
@@ -1,14 +1,73 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { clone } from '../reducers/clone';
+const {dialog} = require('electron').remote;
 
-export default class Clone extends Component {
+const openDir = cb => evt => {dialog.showOpenDialog({properties: ['openDirectory']}, cb);
+};
+
+class Clone extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      userFilePath:''
+    };
+    this.handleCloneClick = this.handleCloneClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(evt) {
+    console.log('THIS IS THE EVENT', evt[0]);
+    const userFilePath = evt[0];
+    this.setState({userFilePath});
+  }
+
+  handleCloneClick(event) {
+    event.preventDefault();
+    clone(this.state.userFilePath, event.target.clonefrom.value);
+  }
+
   render() {
     return (
-      <button className="btn btn-large btn-primary">
-        <span className="icon icon-down-circled icon-text"></span>
-        Clone
-      </button>
+      <div>
+        <button className="btn btn-large btn-default" type="submit" onClick={openDir(this.handleSubmit)}>
+          <span className="icon icon-list-add icon-text"></span>
+            Choose a directory to Clone Into
+        </button>
+
+        <form  className="form-group"  onSubmit={this.handleCloneClick}>
+          {/* <label>Clone into (Local Repo): </label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="haxor99"
+          name="cloneinto"
+        /> */}
+       
+          <label>Clone from (Remote Repo): </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="haxor99"
+            name="clonefrom"
+          />
+          <button className="btn btn-large btn-primary">
+            <span className="icon icon-down-circled icon-text"></span>
+          Clone
+          </button>
+        </form>
+      </div>
+
     );
   }
 }
+
+
+const mapState = ({userPath }) => ({
+  userPath
+});
+
+
+export default connect(mapState, null)(Clone);
 
