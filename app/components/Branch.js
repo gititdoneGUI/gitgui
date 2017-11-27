98c52e609c787771df1b5a737de6119fe92e1716
Arianna Lanz <ariannalanz@gmail.com>
2017-11-27 14:33:02 -0500

    messing around with prompts

--- app/components/Branch.js
+++ app/components/Branch.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {openDialogBox} from '../reducers/branches';
 // import { connect } from 'react-redux';
+// const {dialog} = require('electron').remote
+
+const mapState = ({ repo, status, commit, userPath, branch }) => ({ repo, status, commit, userPath, branch});
+const mapDispatch = (dispatch) => {
+  return {
+    openDialogBox: () =>
+      dispatch(openDialogBox())
+  };
+};
+
 
 export default class Branch extends Component {
   render() {
     return (
-      <button className="btn btn-large btn-primary">
+      <button className="btn btn-large btn-primary"  onClick={this.props.openDialogBox}>
         <span className="icon icon-flow-branch icon-text"></span>
         Branch
       </button>
     );
   }
 }
+
+export default connect(mapState, mapDispatch)(Branch);
 
--- app/reducers/branches.js
+++ app/reducers/branches.js
@@ -1,3 +1,5 @@
+const prompt = require('electron-prompt');
+
 const ADD_BRANCH = 'ADD_BRANCH';
 const DELETE_BRANCH = 'DELETE_BRANCH';
 const GET_BRANCH = 'GET_BRANCH'; 
@@ -5,6 +7,28 @@ const GET_BRANCH = 'GET_BRANCH';
 export const addBranch = branch =>  ({type: ADD_BRANCH, branch});
 export const deleteBranch = branch =>  ({type: DELETE_BRANCH, branch});
 export const getBranch = branch => ({type: GET_BRANCH, branch});
+
+
+export const openDialogBox = () => {prompt ({
+  title: 'Prompt example',
+  label: 'URL:',
+  value: 'http://example.org',
+  inputAttrs: { // attrs to be set if using 'input'
+    type: 'url'
+  },
+  type: 'input', // 'select' or 'input, defaults to 'input'
+  // selectOptions: { // select options if using 'select' type
+  //     'value 1': 'Display Option 1',
+  //     'value 2': 'Display Option 2',
+  //     'value 3': 'Display Option 3'
+  // }
+})
+  .then((r) => {
+    console.log('result', r); // null if window was closed, or user clicked Cancel
+  })
+  .catch(console.error);
+    
+};
 
 export const checkoutBranch = (path, branchName, startPoint) => (dispatch) => {
   require('simple-git')(`${path}`).checkoutBranch(branchName, startPoint);
