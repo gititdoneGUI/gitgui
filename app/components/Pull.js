df900ee4f64316ea68ec4d7cc9953c3c2dacc187
sreepriya <sreepriyav@live.com>
2017-11-27 13:34:24 -0500

    error working

--- app/components/Pull.js
+++ app/components/Pull.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import { pull } from '../reducers/pull';
 
-export default class Pull extends Component {
+class Pull extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handlePullClick = this.handlePullClick.bind(this);
+  }
+
+  handlePullClick(event) {
+    event.preventDefault();
+    pull(this.props.userPath, event.target.branch.value);
+  }
+
   render() {
     return (
-      <button className="btn btn-large btn-primary">
-        <span className="icon icon-down-circled icon-text"></span>
+
+      <form onSubmit={this.handlePullClick} className="form-group">
+        <label>Branch to pull from : </label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="haxor99"
+          name="branch"
+        />
+        <button className="btn btn-large btn-primary">
+          <span className="icon icon-down-circled icon-text"></span>
         Pull
-      </button>
+        </button>
+      </form>
+
+    
     );
   }
 }
+
+
+const mapState = ({userPath }) => ({
+  userPath
+});
+
+
+export default connect(mapState, null)(Pull);
 
