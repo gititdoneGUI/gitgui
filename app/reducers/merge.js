755b57dc0fdd2f5603025bf77127c7c3ecbd899a
Arianna Lanz <ariannalanz@gmail.com>
2017-11-29 16:44:04 -0500

    fixed merging

--- app/reducers/merge.js
+++ app/reducers/merge.js
@@ -2,24 +2,24 @@ const {dialog} = require('electron').remote;
 import {fetchHistory} from './repo';
 
 
-// export const merge = (path, options) => {
-//   require('simple-git')(`${path}`).merge(options);
-// };
+export const merge = (path, options) => {
+  require('simple-git')(`${path}`).merge(options);
+};
 
-// export const mergeFromTo = (path, from, to) => {
-//   require('simple-git/promise')(`${path}`).mergeFromTo(from, to)
-//     .then(() => console.log('merge complete'))
-//     .catch((err) => openDialogBox(err));
-  
-// };
-
-export const mergeFromTo = (path, from, to) => (dispatch) => {
+export const mergeFromTo = (path, from, to) => {
   require('simple-git/promise')(`${path}`).mergeFromTo(from, to)
-    .then(() =>  dispatch(fetchHistory(`${path}`))
-    )
+    .then(() => console.log('merge complete'))
     .catch((err) => openDialogBox(err));
   
 };
+
+// export const mergeFromTo = (path, from, to) => (dispatch) => {
+//   require('simple-git/promise')(`${path}`).mergeFromTo(from, to)
+//     .then(() =>  dispatch(fetchHistory(`${path}`))
+//     )
+//     .catch((err) => openDialogBox(err));
+  
+// };
 
 function openDialogBox(err) {
   const title = 'Error';
