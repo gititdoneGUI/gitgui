5b34ac3c1167785609cebc542b5e84f301d6c345
Arianna Lanz <ariannalanz@gmail.com>
2017-11-30 11:31:34 -0500

    can now push to new remote

--- app/components/Merge.js
+++ app/components/Merge.js
@@ -164,10 +164,6 @@ class Merge extends Component {
             </button>
           </form>
         }
-        {/* <button className="btn btn-large btn-primary">
-          <span className="icon icon-switch icon-text"></span>
-        Merge
-        </button> */}
       </div>
     );
 
